a6f05b5333a6a22f57f98adb4e9e270bdf40a22f
fulviocanducci <fulviocanducci@hotmail.com>
2017-07-04 17:31:18 -0300

    Add project files.

---
+++ EntityFramework6_2_0_Beta_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework6_2_0_Beta_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
